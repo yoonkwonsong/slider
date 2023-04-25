46bfbcd40b5971ff1e927337edee5024a425fb7b
yoonkwonsong <78392347+yoonkwonsong@users.noreply.github.com>
2023-04-25 13:32:36 +0900

    c

--- app.js
+++ app.js
@@ -1,3 +1,29 @@
-document
-    .querySelectorAll(".image")
-    .forEach((e, i) => (e.style.transform = `translateX(${i}00%)`))
+const slides = document.querySelectorAll(".image")
+const btns = document.querySelectorAll(".btn")
+
+let counter = 0
+
+slides.forEach((slide, index) => {
+    slide.style.left = `${index}00%`
+})
+
+btns.forEach((btn) =>
+    btn.addEventListener("click", (e) => {
+        e.target.classList.contains("btn-start") &&
+            (intervalOne = window.setInterval(transformFn, 1000))
+        e.target.classList.contains("btn-stop") &&
+            window.clearInterval(intervalOne)
+    })
+)
+
+function transformFn() {
+    counter++
+
+    counter > slides.length - 1 && (counter = 0)
+
+    slides.forEach((slide) => {
+        slide.style.transform = `translateX(-${counter * 100}%)`
+    })
+
+    // console.log(counter)
+}
